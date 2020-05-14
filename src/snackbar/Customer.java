20387b28328d53a99d277a609b1580952ba8b641
Kedasha Kerr <kedashakerr@northwesternmutual.com>
2020-05-14 18:08:35 -0500

    complete snack class

---
+++ src/snackbar/Customer.java
@@ -0,0 +1,21 @@
+package snackbar;
+
+/*
+fields:
+- maxId: keep track of last used customer id
+- id: automatically generated
+- name
+- cash on hand
+
+methods:
+- get id
+- set and get name
+- add cash to cash on hand
+- get cash on hand
+- but snacks
+  - Given the total cost of the snacks to be purchased, reduce the cash on hand by that amount
+*/
+
+public class Customer {
+  
+}
---
+++ src/snackbar/VendingMachine.java
@@ -0,0 +1,16 @@
+package snackbar;
+
+/*
+fields:
+- maxId: keep track of last used vending machine id
+- id: automatically generated
+- name
+
+methods:
+- get id
+- set and get name
+*/
+
+public class VendingMachine {
+  
+}
